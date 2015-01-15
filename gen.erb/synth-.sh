c2bab8e6a73767ceb74649405edb210bf90e8aa2
Abram Hindle <abram.hindle@ualberta.ca>
2015-01-15 15:23:38 -0700

    removed tutorial added synth-.sh

--- gen.erb/synth-.sh
+++ gen.erb/synth-.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #source ~/cloudorchestra/fm-lp-multi-host-example/bashrc
-cd <% "#{@syncdir}/#{@instrument}" %> ~/cloudorchestra/fm-lp-multi-host-example
+cd <% "#{@syncdir}/#{@instrument}" %> 
 cd <% "#{@module}" %>
 <% @command %>
 
